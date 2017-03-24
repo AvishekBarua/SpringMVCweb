5297da3fc1855c2d441ca1427c1538a2461b47e3
AvishekBarua <avishek_barua@yahoo.com>
2017-03-24 22:57:05 +0200

    files are added

---
+++ src/main/java/com/karnafully/Mvc.java
@@ -0,0 +1,19 @@
+package com.karnafully;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/")
+public class Mvc {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(ModelMap model){
+        model.addAttribute("message", "Spring MVC XML Config Example");
+        return "index";
+    }
+
+}
